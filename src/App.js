a7962099442c262f77962b96b2a6faf82664604b
qiui-yangqi <76548879+qiui-yangqi@users.noreply.github.com>
2021-03-09 21:33:52 +0800

    提示 初步构建

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginBox from './component/loginBox/LoginBox'
-import { ModalProvider } from 'react-use-modal'
-function App() {
-  return (
-    <ModalProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React ha ha
-        </a>
-        <LoginBox></LoginBox>
-        </header>
+import ShowTips from './component/showTips/showTips'
+import { useState, useEffect } from 'react';
 
-      </div>
-    </ModalProvider>
+
+function App() {
+  const [isShowTips, setIsShowTips] = useState(false)
+  const [tipText, setTipText] = useState('')
+
+  useEffect(() => {
+    setIsShowTips(false)
+
+    setTipsTexFN = async (tipTexts) => {
+      await setTipText(tipTexts)
+      setIsShowTips(true)
+      setTimeout(() => {
+        alert(tipText, isShowTips)
+
+      }, 1000);
+    }
+
+    return
+
+  }, [])
+
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ShowTips.Provider value={{ setTipsTexFN: setTipsTexFN }}>
+          <LoginBox></LoginBox>
+        </ShowTips.Provider>
+      </header>
+
+    </div>
   );
 }
 
