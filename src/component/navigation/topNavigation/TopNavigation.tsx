90fd1e634afb0db8b63985fd0c5f5f106d44b8ce
yangQi <906355283@qq.com>
2021-03-17 17:57:18 +0800

    登录注册

--- src/component/navigation/topNavigation/TopNavigation.tsx
+++ src/component/navigation/topNavigation/TopNavigation.tsx
@@ -1,17 +1,22 @@
+/*
+ * @Author: yangQi
+ * @LastEditors:  
+ * @Date: 2021-03-15 10:03:00
+ * @LastEditTime: 2021-03-17 16:43:55
+ * @Description:  
+ * @FilePath: /client/src/component/navigation/topNavigation/TopNavigation.tsx
+ */
 
 import './TopNavigation.css'
 import '../../../publishCss.css'
-export default function TopNavigation(props: any) {
+export default function åTopNavigation(props: any) {
 
     return (
 
         <div className="topNavigation">
             <p className="XXXFont" style={{
-                color: 'black',
-                fontFamily:'KaiTi'
-            }}> wepo </p>
+            }}>w&nbsp;e&nbsp;p&nbsp;o</p>
             <div className="findBigBox">
-
             <input type="text" className="findBox" />
             </div>
         </div>
