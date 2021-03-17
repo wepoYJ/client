9e0c8faddb924a01ccb908d06ca85c8b68fb3f24
qiui-yangqi <76548879+qiui-yangqi@users.noreply.github.com>
2021-03-17 21:53:22 +0800

    调试网络

--- src/component/req/request.ts
+++ src/component/req/request.ts
@@ -1,67 +1,45 @@
-// import { useModal } from 'react-use-modal'
+
 import qs from "qs"
-// import toas from '../toas'
 import axios from 'axios'
-interface reqReturn {
+interface reqReturn<T> {
     code: number,
     msg: string,
-    data: any
+    data: T
 }
-
 
 const host: string = 'http://8.136.4.252:3000/'
 let token: string = ''
 
-const req_Post_Normal = function (url: string, data: any): Promise<reqReturn> {
-    url = host + url
-    console.log(url)
+let axiosClient = axios.create({
+    baseURL: host,
+    headers: {
+        token:token,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+})
 
-    return new Promise((resolve, reject) => {
-        axios.request({
-            method: 'POST',
-            data: qs.stringify(data),
-            headers: {
-                // token: token
-            },
-        })
-            .then((res: any) => {
-                console.log(res)
-                if (res.code === 200) {
-                    resolve(res.data)
+const req_Post_Normal = async function <T> (url: string, data: any): Promise<T | null> {
+    let resp
+    try {
+        resp = await axiosClient.post(url, qs.stringify(data), {})
+        if (resp.status=== 200) {
+            let { code, msg, data } = resp.data as reqReturn<T>
+            
+            if (code === 0) {
+                // 成功
+                return Promise.resolve(data)
+            } else {
+                // 失败
+                if (msg) {
+                    // 有错误提示
                 }
-                else {
-                    // toas.error(res.msg, 300)
-                    reject({ code: res.code, msg: res.msg })
-                }
-
-            })
-            .catch((e) => {
-                // toas.error(e, 300)
-                // console.log(e)
-            })
-    })
-
-
-    // return new Promise((resolve, reject) => {
-    //     fetch(host + url, {
-    //         method: 'POST',
-    //         headers: {
-    //             Accept: 'application/json',
-    //             'Content-Type': 'application/json',
-    //             token: token
-    //         },
-    //         body: data
-    //     }).then((res: any) => {
-    //         return res.text()
-    //     }).then((res: reqReturn) => {
-    //         resolve(res)
-    //     }).catch((e: any) => {
-    //         console.warn(e)
-    //         reject(e)
-    //     })
-    // })
-
-
+                return Promise.resolve(null)
+            }
+        }
+    } catch (e) {
+        console.error(e)
+    }
+    return Promise.resolve(null)
 }
 
 export {
--- src/net/request.ts
+++ src/net/request.ts
@@ -10,46 +10,74 @@
 // import { Modal } from 'antd'
 import qs from "qs"
 // import toas from '../toas'
-import axios from 'axios'
+// import axios from 'axios'
 interface reqReturn<T> {
     code: number,
     msg: string,
     data: T
 }
 
-const host: string = 'http://8.136.4.252:3000/'
+const host: string = 'http://8.136.4.252:5000/'
 let token: string = ''
 
-let axiosClient = axios.create({
-    baseURL: host,
-    headers: {
-        token:token,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-})
+// let axiosClient = axios.create({
+//     baseURL: host,
+//     headers: {
+//         token: token,
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//     }
+// })
 
-const req_Post_Normal = async function <T> (url: string, data: any): Promise<T | null> {
-    let resp
-    try {
-        resp = await axiosClient.post(url, qs.stringify(data), {})
-        if (resp.status=== 200) {
-            let { code, msg, data } = resp.data as reqReturn<T>
-            
-            if (code === 0) {
-                // 成功
-                return Promise.resolve(data)
-            } else {
-                // 失败
-                if (msg) {
-                    // 有错误提示
+const req_Post_Normal = async function <T>(url: string, data: any): Promise<string | null> {
+    // let resp
+    // try {
+    //     resp = await axiosClient.post(url, qs.stringify(data), {})
+    //     if (resp.status=== 200) {
+    //         let { code, msg, data } = resp.data as reqReturn<T>
+
+    //         if (code === 0) {
+    //             // 成功
+    //             return Promise.resolve(data)
+    //         } else {
+    //             // 失败
+    //             if (msg) {
+    //                 // 有错误提示
+    //             }
+    //             return Promise.resolve(null)
+    //         }
+    //     }
+    // } catch (e) {
+    //     console.error(e)
+    // }
+    // return Promise.resolve(null)
+    return new Promise((resolve, reject) => {
+        fetch(host + url, {
+            headers: {
+                token: token,
+                'content-type': 'application/json'
+            },
+            body: qs.stringify(data),
+            method: "POST",
+            mode:'no-cors',
+            cache: 'no-cache',
+        })
+            .then((res) => {
+                return (res.text())
+            })
+            .then((res: any) => {
+                return JSON.parse(res)
+            })
+            .then((res: reqReturn<T>) => {
+                console.log(res.code, res.data, res.msg)
+                if (res.code === 0) {
+                    return resolve(res.msg)
                 }
-                return Promise.resolve(null)
-            }
-        }
-    } catch (e) {
-        console.error(e)
-    }
-    return Promise.resolve(null)
+                reject(res.msg)
+            })
+            .catch((e) => {
+                reject(e)
+            })
+    })
 }
 
 export {
