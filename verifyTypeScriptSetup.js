79564cb3735a1da8f360a26632aa37f5f590b57a
yangQi <906355283@qq.com>
2021-03-17 16:02:01 +0800

    更改tyscript缺失问题

---
+++ verifyTypeScriptSetup.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: yangQi
+ * @LastEditors:  
+ * @Date: 2021-03-17 15:59:39
+ * @LastEditTime: 2021-03-17 15:59:50
+ * @Description:  
+ * @FilePath: /client/verifyTypeScriptSetup.js
+ */
+"use strict";
+function verifyTypeScriptSetup () {}
+module.exports = verifyTypeScriptSetup;
